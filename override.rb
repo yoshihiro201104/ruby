4bc4381ae4666d0fe38108701bf1c6322670016c
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-04-22 23:50:34 +0000

    add

--- override.rb
+++ override.rb
@@ -6,10 +6,13 @@ end
 
 class Bus < Car
   def run(distance)
-    puts "バスで#{distance}キロ走ります。"
+    super
+    puts "30人を乗せて、走っています。"
   end
 end
 
 bus = Bus.new
 bus.run(5)
 
+car = Car.new
+car.run(5)
