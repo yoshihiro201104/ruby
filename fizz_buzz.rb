bf199a4d053e546fdd80e020ef38d1519639d9b1
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-04 01:59:22 +0000

    add

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,18 +1,9 @@
 def fizz_buzz(number)
-  if number % 15 == 0 
-    puts "FizzBuzz"
-  elsif number % 3 == 0
-    puts "FizzBuzz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  else
-    number.to_s
-  end
+  
 end
 
-  puts "数字を入力してください"
+puts "数字を入力してください。"
+input = gets.to_i
 
-  input = gets.to_i
-
-  puts "結果は..."
-  puts fizz_buzz(input)
+puts "結果は..."
+puts fizz_buzz(input)
