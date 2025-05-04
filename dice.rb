17642807edaa7bbffa4a7049574e1563b7afda34
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-04 02:08:30 +0000

    add

--- dice.rb
+++
@@ -1,6 +0,0 @@
-dice = 0
-
-while dice != 9 do
-  dice = rand(1..6)
-  puts dice
-end
--- lesson5.rb
+++
@@ -1,9 +0,0 @@
-total_money = 90
-
-if total_money > 100
-  puts "みかんを購入。所持金に余りあり。"
-elsif total_money == 100
-  puts "みかんを購入。所持金は0円。"
-else 
-  puts "みかんを購入することができません。"
-end
