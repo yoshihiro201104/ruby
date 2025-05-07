cd021d1feeb85b16342541899dc9d77c381cc58f
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-07 02:54:02 +0000

    add

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -9,7 +9,17 @@ while i <= input do
 
   a = gets.to_i
   b = gets.to_i
-  
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
   puts "計算結果を出力します"
   puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+
+  i +=1
 end
+
+puts "計算を終了します"
