a4a0e4e5eaed1724b9164f8fe772afd26e7aa600
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-08 01:41:33 +0000

    add

--- new.rb
+++ new.rb
@@ -3,11 +3,15 @@ class Car
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
-  
+
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
+
+
+car = Car.new
+car.turn("右")
 
 car = Car.new
 car.run(5)
