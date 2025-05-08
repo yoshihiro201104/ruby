69daa1a6270278dfa0377e86d9916c1da476ab01
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-08 02:18:45 +0000

    add

--- new.rb
+++ new.rb
@@ -1,31 +1,7 @@
-class Car
-
-  def move(direction,distance)
-    self.turn(direction)
-    self.run(distance)
-  end
-
-
-  def turn(direction)
-    puts "#{direction}に曲がります。"
-  end
-
-  def run(distance)
-    puts "車で#{distance}キロ走ります。"
+class Car 
+  def.self.turn(direction)
+    puts "#{direction}に曲がります"
   end
 end
 
-
-car = Car.new
-car.move("右",5)
-
-
-
-#　クラスメソッド
-class Car
-  def Car.run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-end
-
-Car.run(10)
+Car.turn("右")
