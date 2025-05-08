c4e2d387a206de7ad9e2d10d6fc3c2bd7538aea9
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-08 00:01:00 +0000

    add

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -1,12 +1,12 @@
 puts "計算をはじめます"
-puts "何回繰り返しますか？"
+puts "何回繰り返しますか"
+
 input = gets.to_i
 
-i =1
+i = 1
 while i <= input do
   puts "#{i}回目の計算"
   puts "2つの値を入力してください"
-
   a = gets.to_i
   b = gets.to_i
 
@@ -19,7 +19,9 @@ while i <= input do
   puts "#{a}*#{b}=#{a * b}"
   puts "#{a}/#{b}=#{a / b}"
 
-  i +=1
+  i += 1
 end
+
+
 
 puts "計算を終了します"
