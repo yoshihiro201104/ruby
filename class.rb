6048dbfb65fad300336797a4f467665d709658b2
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-05-08 01:55:58 +0000

    add

--- class.rb
+++ class.rb
@@ -5,3 +5,12 @@ puts array.join("*")
 string = "Hello"
 puts string.class
 puts string.join("*")
+
+#　クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
